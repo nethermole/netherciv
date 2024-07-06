d0bf1e8e0f7b7d9d46298655692229c9a2aba08a
Kyle Hersey <nethermole@gmail.com>
2024-07-06 13:34:15 -0700

    got the constructor to work. better comments

--- Source/netherciv/ProceduralGlobe.cpp
+++ Source/netherciv/ProceduralGlobe.cpp
@@ -25,45 +25,52 @@ void AProceduralGlobe::GenerateWorld()
 	triangles = dcel->triangles;
 }
 
+
+//1) For each endpoint, create a vertex.
+//2)For each input segment, create two half-edges, and assign their tail vertices and twins. Pick one of the half-edges and assign it as the representative for the endpoints.
+//3) Subdivide n times
+	//3a) Generate new vertices
+	//3b) For each input segment, create two half-edges, and assign their tail vertices and twins. Pick one of the half-edges and assign it as the representative for the endpoints.
+//4)
+	//4a) For each endpoint, sort the half - edges whose tail vertex is that endpoint in clockwise order.
+	//4b) For every pair of half - edges e1, e2 in clockwise order, assign e1->twin->next = e2 and e2->prev = e1->twin
+//5)For every cycle, allocate and assign a face structure.
 void AProceduralGlobe::CreateGlobeDcel(int subdivisions)
 {
 	dcel = new DoublyConnectedEdgeList();
 
-	//1) For each endpoint, create a vertex.
-	dcel->LoadIcosahedronCartesianCoordinates();
+	dcel->LoadIcosahedronCartesianCoordinates();	//1
+	dcel->CalculateHalfEdges();		//2
 
-	//2)For each input segment, create two half-edges, and assign their tail vertices and twins. Pick one of the half-edges and assign it as the representative for the endpoints.
-	dcel->CalculateHalfEdges();
-
-	//3) Subdivide n times
-	for (int subdivCount = 0; subdivCount < subdivisions; subdivCount++) {
-		//3a) Generate new vertices
+	for (int subdivCount = 0; subdivCount < subdivisions; subdivCount++) {	//3
 		dcel->Subdivide();
-		//3b) For each input segment, create two half-edges, and assign their tail vertices and twins. Pick one of the half-edges and assign it as the representative for the endpoints.
 		dcel->CalculateHalfEdges();
 	}
 
-	//4)For each endpoint, sort the half - edges whose tail vertex is that endpoint in clockwise order.
-	//	For every pair of half - edges e1, e2 in clockwise order, assign e1->twin->next = e2 and e2->prev = e1->twin
-	dcel->DoClockwiseAssignment(false);
-
-	//6)For every cycle, allocate and assign a face structure.
-	dcel->GetFacesFromHalfEdges(dcel->halfEdgesBetweenVertices);
+	dcel->DoClockwiseAssignment(false);		//4
+	dcel->GetFacesFromHalfEdges(dcel->halfEdgesBetweenVertices);	//5
 
 //To hexes
 	//Get center of adjacent faces, make new list of vertices for the hex globe
 	dcel->hexGlobeVertices = dcel->GenerateHexGlobeVertices();
 
-	//then do "3 adjacents" for the map
-	dcel->hexGlobeAdjacencies = dcel->GetHexGlobeAdjacencies(dcel->hexGlobeVertices);
-	dcel->halfEdgesBetweenVertices = dcel->GetHalfEdgesBetweenVertices(dcel->hexGlobeAdjacencies);
-	dcel->DoClockwiseAssignment(true);
-	dcel->GetFacesFromHalfEdges(dcel->halfEdgesBetweenVertices);
-	UE_LOG(LogTemp, Display, TEXT("globe faces total = %d"), dcel->faces.Num());
+	DoublyConnectedEdgeList* hexDcel = new DoublyConnectedEdgeList();
+	hexDcel->hexGlobeVertices = dcel->hexGlobeVertices;
+	hexDcel->originalVertices = dcel->originalVertices;
+
+	//calculate half edges
+	hexDcel->hexGlobeAdjacencies = hexDcel->GetHexGlobeAdjacencies(hexDcel->hexGlobeVertices);
+	hexDcel->halfEdgesBetweenVertices = hexDcel->GetHalfEdgesBetweenVertices(hexDcel->hexGlobeAdjacencies);
+
+	hexDcel->DoClockwiseAssignment(true);
+	hexDcel->GetFacesFromHalfEdges(hexDcel->halfEdgesBetweenVertices);
+	UE_LOG(LogTemp, Display, TEXT("globe faces total = %d"), hexDcel->faces.Num());
 
 
 	//Prepare verticesLocations and triangles
-	dcel->PrepareVerticeLocationsAndTriangles();
+	hexDcel->PrepareVerticeLocationsAndTriangles();
+
+	dcel = hexDcel;
 }
 
 
